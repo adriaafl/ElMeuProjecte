9c79f16f9be0abaac1ff418c84f40b57e5bf8818
Adria <afabregas.dam@institutcampalans.net>
2025-05-29 19:46:37 +0200

    Primera versió del projecte

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int contador = 0;
+
+        for (int i = 1; i < 200; i++)
+        {
+            if (i % 7 == 0)
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine("Hi ha " + contador + " nombres menors de 200 que són múltiples de 7.");
+    }
+}
+
+
